f98ef4092272160c1131edd7ba85b1882f9d414a
dar8900 <dar8900@gmail.com>
2020-03-15 12:37:38 +0100

    -Inseriti file di libreria per la gestione di un segmentdisplay con max7912

---
+++ SegmentDisplays.cpp
@@ -0,0 +1,53 @@
+#include "SegmentDisplays.h"
+
+MAX7219 display (N_CHIPS, CS);
+
+
+void SegDispBegin()
+{
+	display.begin();
+}
+
+
+void SegDispPrintInt(uint32_t Number)
+{
+	char numstr[MAX_CHARS];
+	snprintf(numstr, MAX_CHARS, "%8ld", Number);
+	display.sendString(numstr);		
+}
+
+void SegDispPrintInt(int32_t Number)
+{
+	char numstr[MAX_CHARS];
+	snprintf(numstr, MAX_CHARS, "%8ld", Number);
+	display.sendString(numstr);		
+}
+
+void SegDispPrintFl(float Number)
+{
+	char numstr[MAX_CHARS];
+	dtostrf(Number, MAX_CHARS, 2, numstr);
+	display.sendString(numstr);	
+}
+
+void SegDispReset()
+{
+	display.end();
+	display.begin();
+	display.setIntensity(MIN_INTENSITY);
+}
+
+void SegDispPrintString(char *string)
+{
+	
+	if(strlen(string) <= 8)
+		display.sendString(string);
+	else
+		display.sendString("--------");
+}
+
+void SegDispSetIntensity(uint8_t Intensity)
+{
+	if(Intensity >= 0 && Intensity <= 15)
+		display.setIntensity(Intensity);
+}
---
+++ SegmentDisplays.h
@@ -0,0 +1,24 @@
+#ifndef SEGMENT_DISPLAYS_H
+#define SEGMENT_DISPLAYS_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <bitBangedSPI.h>
+#include <MAX7219.h>
+#include <stdint.h>
+
+
+#define N_CHIPS				1
+#define CS				   10
+#define MIN_INTENSITY	   14
+#define MAX_CHARS			9
+
+void SegDispBegin();
+void SegDispPrintInt(uint32_t Number);
+void SegDispPrintInt(int32_t Number);
+void SegDispPrintFl(float Number);
+void SegDispPrintString(char *string);
+void SegDispReset();
+void SegDispSetIntensity(uint8_t Intensity);
+
+#endif
